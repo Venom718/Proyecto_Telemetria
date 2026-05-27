e4bc2ae8bfff52869b8186f03c01a4b22292a519
Rous_345 <venomh718@gmail.com>
2026-05-26 20:28:40 -0600

    Agregacion de funciones como la de sensor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "sensor.h"
 
 int main() {
     std::cout << "Iniciando sistema de telemetria..." << std::endl;
+    std::cout << "Lectura actual: " << leerTemperatura() << " grados." << std::endl;
     return 0;
 }
